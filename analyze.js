a2c6d5c50058446196e856de9cd8cf134d5d2d8c
引尻貴俊 <hikijiri@hakobune.co.jp>
2021-05-19 01:18:35 +0900

    リファクタリング

--- analyze.js
+++ analyze.js
@@ -60,3 +60,45 @@ function countUPTotalPageView(result) {
 function removeQueryParameters(url) {
     return url.replace(/\?[^ ]* /, ' ');
 }
+
+
+class analyzeNginxLog {
+    // ファイルの読み取りと解析
+
+    // コンストラクタ
+    constructor(logfile) {
+        // const logfile = process.argv[2];
+        const arr = require("fs").readFileSync(logfile).toString().trim().split("\n");
+        const regex = /^(.+) - (.+) \[(.+)\] "([^\"]+)" (\d+) (\d+) "([^\"]+)" "([^\"]+)"$/;
+        this.result = this.analyze(arr);
+    }
+
+    // 解析のためのメソッド
+    analyze(arr) {
+        let pages = [];
+        for (let len = arr.length, i = 0; i < len; i++) {
+
+            const line = arr[i].trim();
+            let [
+                src, // 元々の入力データ
+                ip, // リモートIPアドレス
+                user, // リモートユーザ
+                time, // 日時
+                page, // HTTPメソッドとアクセスされたパス
+                code, // ステータスコード
+                size, // データサイズ
+                referer, // Referer
+                ua, // User-Agent
+            ] = line.match(regex);
+
+        //     const m = pages.find(x => x.ip === ip);
+        //     if (m) {
+        //         m.count += 1;
+        //     } else {
+        //         pages.push({ip, count: 1});
+        //     }
+        }
+
+        return line;
+    }
+}
